86564be8d0b890e9ec7c8b541d85356b4a3854bc
freenav <80451923+freenav@users.noreply.github.com>
2022-12-17 23:03:43 +0100

    Update Small Detail

--- airports.js
+++ airports.js
@@ -18,28 +18,147 @@
 */
 
 
-module.exports = {
-    "airports": {
-        "EDDS": {
-            "GND": {
-                "Ground": "https://aip.dfs.de/BasicIFR/pages/P00C04.html",
-                "Apron": "https://aip.dfs.de/BasicIFR/pages/P00C05.html",
-            },
-            "SID": {
-                "Rwy 07": "https://aip.dfs.de/BasicIFR/pages/P00C1E.html",
-                "Rwy 25": "https://aip.dfs.de/BasicIFR/pages/P00C1A.html",
-            },
-            "STAR": {
-                "STAR 07/25": "https://aip.dfs.de/BasicIFR/pages/P00C0D.html",
-                "Transition to Final 07": "https://aip.dfs.de/BasicIFR/pages/P00C0E.html",
-                "Transition to Final 25": "https://aip.dfs.de/BasicIFR/pages/P00C0F.html",
-                "Transition to Final 07 (CDO)": "https://aip.dfs.de/BasicIFR/pages/P00C10.html",
-                "Transition to Final 25 (CDO)": "https://aip.dfs.de/BasicIFR/pages/P00C11.html",
-            },
-            "IAC": {
-                "ILS 07": "https://aip.dfs.de/BasicIFR/pages/P00C13.html",
-                "ILS 25": "https://aip.dfs.de/BasicIFR/pages/P00C12.html",
-            }
+airports = {
+    "EDDS": {
+        "GND": {
+            "Ground": "https://aip.dfs.de/BasicIFR/print/AD/2FCFEB82DB1B5599EC507A343687EF62/AD%202%20EDDS%202-5",
+            "Apron": "https://aip.dfs.de/BasicIFR/print/AD/EBA9228E63C4225159D0D6A688145B11/AD%202%20EDDS%202-7"
+        },
+        "SID": {
+            "Rwy 07": "https://aip.dfs.de/BasicIFR/print/AD/E573E0744439219F961AB09D8930A01A/AD%202%20EDDS%205-7-7",
+            "Rwy 25": "https://aip.dfs.de/BasicIFR/print/AD/67E5FE4B7A334511F8D5F762DCAC77FA/AD%202%20EDDS%205-7-3"
+        },
+        "STAR": {
+            "STAR 07/25": "https://aip.dfs.de/BasicIFR/print/AD/89976781E0F24B5940CB570E7F486936/AD%202%20EDDS%203-1-2",
+            "Transition to Final 07": "https://aip.dfs.de/BasicIFR/print/AD/33469952090B3F77F504A193B6DEE7BC/AD%202%20EDDS%203-1-3",
+            "Transition to Final 25": "https://aip.dfs.de/BasicIFR/2022DEC01/pages/1F998A1C3BEB157CFDCD4D85EEF3023C.html",
+            "Transition to Final 07 (CDO)": "https://aip.dfs.de/BasicIFR/print/AD/D8A61CE756E56ECF0C716160904E017A/AD%202%20EDDS%203-1-5",
+            "Transition to Final 25 (CDO)": "https://aip.dfs.de/BasicIFR/print/AD/9E754F9223FDFDCD018CF64DEADC8D9E/AD%202%20EDDS%203-1-6"
+        },
+        "IAC": {
+            "ILS 07": "https://aip.dfs.de/BasicIFR/print/AD/88AE6EFF9305B6C4E045EB8E571CC3C7/AD%202%20EDDS%204-2-2",
+            "ILS 25": "https://aip.dfs.de/BasicIFR/print/AD/5C6FA663B76D1C589489B1BD011A6271/AD%202%20EDDS%204-2-1"
+        }
+    },
+    "EDDN": {
+        "GND": {
+            "Ground": "https://aip.dfs.de/basicIFR/print/AD/673D19656668E0D4D779A96B497F5019/AD%202%20EDDN%202-5",
+            "Apron": "https://aip.dfs.de/basicIFR/print/AD/D3BE533DF1429B3FA4E0A32615066618/AD%202%20EDDN%202-7",
+        },
+        "SID": {
+            "Rwy 10 VOR (NUB 1A)": "https://aip.dfs.de/basicIFR/print/AD/D145DA4BD32B19853C9E19760E462461/AD%202%20EDDN%205-7-3",
+            "Rwy 28 VOR (NUB 2B)": "https://aip.dfs.de/basicIFR/print/AD/E027352B37CC020BFD68CA93A6280F1D/AD%202%20EDDN%205-7-4",
+            "Rwy 10 RNAV": "https://aip.dfs.de/basicIFR/print/AD/DCB1D4013238CB8C975C90853055077D/AD%202%20EDDN%205-7-5",
+            "Rwy 28 RNAV": "https://aip.dfs.de/basicIFR/print/AD/40DEE9B1A5A8E172089EBB2A69DA4B10/AD%202%20EDDN%205-7-8"
+        },
+        "STAR": {
+            "RNAV Rwy 10": "https://aip.dfs.de/basicIFR/print/AD/08856D6031AB363F6D3000F45C46259A/AD%202%20EDDN%203-1-1",
+            "RNAV Rwy 28": "https://aip.dfs.de/basicIFR/print/AD/CE9277E2F1B51D1EACE3E160B746A80B/AD%202%20EDDN%203-1-4"
+
+        },
+        "IAC": {
+            "ILS 10": "https://aip.dfs.de/basicIFR/print/AD/9540DD43E21060618FE89C0D17D97677/AD%202%20EDDN%204-2-1",
+            "ILS 28": "https://aip.dfs.de/basicIFR/print/AD/468B15BD8A2075CBE597D4B902205A06/AD%202%20EDDN%204-2-2"
+        }
+    },
+    "EDDM": {
+        "GND": {
+            "Ground": "https://aip.dfs.de/basicIFR/print/AD/5D88ACC2C707BC07D43CB05DFD7FF5F5/AD%202%20EDDM%202-5",
+            "Apron": "https://aip.dfs.de/basicIFR/print/AD/5D0F7F6A9F9DEDA91C9D0385F3F89E54/AD%202%20EDDM%202-9",
+        },
+        "SID": {
+            "08L North GIVMI/RIDAR": "https://aip.dfs.de/basicIFR/print/AD/43373E99CE5371A990C41C711E778B58/AD%202%20EDDM%205-7-5",
+            "08L South BIBAG/KIRDI/ROTAX/VAVOR/TURBU/TULSI/MERSI/MERSI/OBAXA/ALG/OLASO": "https://aip.dfs.de/basicIFR/print/AD/F6FC6B91F2E8A6AF4F4AA71E7CF94B72/AD%202%20EDDM%205-7-7",
+            "08L North RNAV ANKER/EVIVA/INPUD": "https://aip.dfs.de/basicIFR/print/AD/C067212F75B9FFCFACD3C5A2F3B2C307/AD%202%20EDDM%205-7-9",
+            "08L North RNP AKINI": "https://aip.dfs.de/basicIFR/print/AD/E842C5FE16240B90C5AF3EB289EF640D/AD%202%20EDDM%205-7-11",
+            "08R North RIDAR/GIVME": "https://aip.dfs.de/basicIFR/print/AD/AD6D1FF07E4388BA943F25D2B9C86807/AD%202%20EDDM%205-7-17",
+            "08R South BIBAG/KIRDI/ROTAX/VAVOR/TURBU/TULSI/MERSI/OBAXA/MERSI/ALG/OLASO": "https://aip.dfs.de/basicIFR/print/AD/CC4C99A7D515FF40432316182BAA7A6C/AD%202%20EDDM%205-7-19",
+            "08R North RNAV ANKER/EVIVA/INPUD": "https://aip.dfs.de/basicIFR/print/AD/77308596FA2903EA8DC86B704E830CAF/AD%202%20EDDM%205-7-21",
+            "08R North RNP AKINI": "https://aip.dfs.de/basicIFR/print/AD/96E979EE2E2F6EFBEFAB349A46DAD9BD/AD%202%20EDDM%205-7-23",
+            "26L North MIQ/GIVMI/RIDAR": "https://aip.dfs.de/basicIFR/print/AD/DB6648CF2D0D336F6792D19D77F9B39B/AD%202%20EDDM%205-7-29",
+            "26L South OLASO/ALG/MERSI/OBAXA/TULSI/TURBU/VAVOR/ROTAX/KIRDI/BIBAG": "https://aip.dfs.de/basicIFR/print/AD/1FD6C9396F10C0399D8AE1A134B0DD44/AD%202%20EDDM%205-7-31",
+            "26L North RNAV ANKER/EVIVA/INPUD": "https://aip.dfs.de/basicIFR/print/AD/987E3260AAE0C4FB9553D441B388EA6A/AD%202%20EDDM%205-7-33",
+            "26L North RNP AKINI": "https://aip.dfs.de/basicIFR/print/AD/A792D32D03DB958F75AF9EED6D582400/AD%202%20EDDM%205-7-35",
+            "26R North RIDAR/GIVMI/MIQ": "https://aip.dfs.de/basicIFR/print/AD/0715D1EE4B85A6FD7D1F458B7EA83F28/AD%202%20EDDM%205-7-41",
+            "26R South OLASO/ALG/MERSI/OBAXA/TULSI/TURBU/VAVOR/ROTAX/KIRDI/BIBAG": "https://aip.dfs.de/basicIFR/print/AD/ED98031A5072FCF237322B4F83049714/AD%202%20EDDM%205-7-43",
+            "26R North RNAV INPUD/EVIVA/ANKER": "https://aip.dfs.de/basicIFR/print/AD/029D7FCBD4375C4991D8DA8DD029EDBB/AD%202%20EDDM%205-7-45",
+            "26R North RNP AKINI": "https://aip.dfs.de/basicIFR/print/AD/CCF38DB611460EEA9BAA2838603900F8/AD%202%20EDDM%205-7-47",
+
+
+        },
+        "STAR": {
+            "STAR 08L/R": "https://aip.dfs.de/basicIFR/print/AD/126D5F9BA864EC9AA50EC8C6F1B609E4/AD%202%20EDDM%203-1-1",
+            "STAR 26L/R": "https://aip.dfs.de/basicIFR/print/AD/B34D30643399AAD8BC7892E6B0DBCE40/AD%202%20EDDM%203-1-3",
+            "TTF ROKIL/LANDU 08": "https://aip.dfs.de/basicIFR/print/AD/4C4DD63D010CE0D763BB9ACD2BB28BB7/AD%202%20EDDM%203-1-5",
+            "TTF BETOS/NAPSA 08": "https://aip.dfs.de/basicIFR/print/AD/5EB7ECC8A7A87F0DED063024F0534543/AD%202%20EDDM%203-1-6",
+            "TTF ROKIL/LANDU 26": "https://aip.dfs.de/basicIFR/print/AD/AE0492D7F24EB782FA0FBDEFD2B73D97/AD%202%20EDDM%203-1-7",
+            "TTF BETOS/NAPSA 26": "https://aip.dfs.de/basicIFR/print/AD/37BE52FD848623FCF3D1E18AEBFD61AF/AD%202%20EDDM%203-1-8",
+            "TTF (CDO) LULAR/VESUB/NIRGO 2C (1)": "https://aip.dfs.de/basicIFR/print/AD/52DD75BFB6C25E3EF026C26AF3E60D94/AD%202%20EDDM%203-1-9",
+            "TTF (CDO) LULAR/VESUB/NIRGO 2C (2)": "https://aip.dfs.de/basicIFR/print/AD/EB3A46F8770FAF5B6EFBF3F1E9B786FD/AD%202%20EDDM%203-1-10",
+            "TTF (CDO) ELMOX 1D (1)": "https://aip.dfs.de/basicIFR/print/AD/6585C881B01D1AFB70C1C746D67ED7C0/AD%202%20EDDM%203-1-11",
+            "TTF (CDO) ELMOX 1D (2)": "https://aip.dfs.de/basicIFR/print/AD/559E8A8165D8725E5DA67D8E06B5059E/AD%202%20EDDM%203-1-12",
+            "TTF (CDO) BAGSI/VATET/RASTA 1C (1)": "https://aip.dfs.de/basicIFR/print/AD/142E344A87F17B871EBED47F6BF0D00B/AD%202%20EDDM%203-1-13",
+            "TTF (CDO) BAGSI/VATET/RASTA 1C (2)": "https://aip.dfs.de/basicIFR/print/AD/572274A163240ED4C567DC7D599199D1/AD%202%20EDDM%203-1-14"
+        },
+        "IAC": {
+            "ILS 08L": "https://aip.dfs.de/basicIFR/print/AD/221FCEEFC1EA8F11BB89110358E6CF5D/AD%202%20EDDM%204-2-1",
+            "ILS 08R": "https://aip.dfs.de/basicIFR/print/AD/66250480578FDE2D8F00F4DD1EA24B6A/AD%202%20EDDM%204-2-2",
+            "ILS 26L": "https://aip.dfs.de/basicIFR/print/AD/C46F0510BF159A82AAECECD105916C6A/AD%202%20EDDM%204-2-3",
+            "ILS 26R": "https://aip.dfs.de/basicIFR/print/AD/014E1BBBB063B5CD5852E0989CEBA6CE/AD%202%20EDDM%204-2-4"
+        }
+    },
+    "EDDL": {
+        "GND": {
+            "Ground": "https://aip.dfs.de/basicIFR/print/AD/3C32C6F45287F00DF8111063F14AA20F/AD%202%20EDDL%202-5",
+            "Apron": "https://aip.dfs.de/basicIFR/print/AD/1B2728ABF52BD6DB857DCC916B07BA94/AD%202%20EDDL%202-9",
+        },
+        "SID": {
+            "23L": "https://aip.dfs.de/basicIFR/print/AD/173B1DA8A5118334070145A15B21C162/AD%202%20EDDL%205-7-3",
+            "23L RNP (MODRU1K)": "https://aip.dfs.de/basicIFR/print/AD/22C1D46432433F78A4D97798B7FBEE5C/AD%202%20EDDL%205-7-5",
+            "23R": "https://aip.dfs.de/basicIFR/print/AD/4F2071E8AC01C38EFDE7576E676FF2A8/AD%202%20EDDL%205-7-9",
+            "05R": "https://aip.dfs.de/basicIFR/print/AD/69FA43DB4A6B17FA079A1132D01E614E/AD%202%20EDDL%205-7-13",
+            "05L": "https://aip.dfs.de/basicIFR/print/AD/7A2665FB8B3A6771E490FE2670847C89/AD%202%20EDDL%205-7-17",
+
+        },
+        "STAR": {
+            "STAR 05L/05R": "https://aip.dfs.de/basicIFR/print/AD/FBE409CD84AF1FBAA10B457FDCCDFCC0/AD%202%20EDDL%203-1-2",
+            "STAR 23L/23R": "https://aip.dfs.de/basicIFR/print/AD/9A887E295C0B4019191553F8771DB9E0/AD%202%20EDDL%203-1-4",
+            "TTF 05L/05R": "https://aip.dfs.de/basicIFR/print/AD/1B965E8621840A58769D7187A77D75FE/AD%202%20EDDL%203-1-5",
+            "TTF 23L/23R": "https://aip.dfs.de/basicIFR/print/AD/4CF0F1A49728D4DD5D063F9E1C1F43C1/AD%202%20EDDL%203-1-6",
+            "TTF (CDO) 05L/05R": "https://aip.dfs.de/basicIFR/print/AD/A3D1ED277C4CA7774FA6E91DD6A20CF5/AD%202%20EDDL%203-1-7",
+            "TTF (CDO) 23L/23R": "https://aip.dfs.de/basicIFR/print/AD/525F3786A0318D8A47501F4A72CEA7DA/AD%202%20EDDL%203-1-8",
+        },
+        "IAC": {
+            "ILS 05L": "https://aip.dfs.de/basicIFR/print/AD/55760BAB37AB4C6808F9DA3C8B6621CB/AD%202%20EDDL%204-2-4",
+            "ILS 05R": "https://aip.dfs.de/basicIFR/print/AD/CD48B947EF581C860B24732DF34764B2/AD%202%20EDDL%204-2-3",
+            "ILS 23L": "https://aip.dfs.de/basicIFR/print/AD/F4508FECA536F0E8F9482D20A1C3B626/AD%202%20EDDL%204-2-1",
+            "ILS 23R": "https://aip.dfs.de/basicIFR/print/AD/7EBD7DF388D644D22E408E36A69D85C8/AD%202%20EDDL%204-2-2",
+        }
+    },
+    "EDDH": {
+        "GND": {
+            "Ground": "https://aip.dfs.de/basicIFR/print/AD/DA951A3A80BB7FE4D5763A628BFF157C/AD%202%20EDDH%202-5",
+            "Apron": "https://aip.dfs.de/basicIFR/print/AD/8DD319F390C98514BE09A156438886B7/AD%202%20EDDH%202-7",
+        },
+        "SID": {
+            "SID Rwy 05/15/23/33 HAM": "https://aip.dfs.de/basicIFR/print/AD/4BA718395477905649976DB420439DD5/AD%202%20EDDH%205-7-2",
+            "RNAV 05": "https://aip.dfs.de/basicIFR/print/AD/8A3C787922E4AB1E11449435C78CC560/AD%202%20EDDH%205-7-3",
+            "RNAV 15": "https://aip.dfs.de/basicIFR/print/AD/E5721CA4888DB7A3C95099E84A6461CA/AD%202%20EDDH%205-7-6",
+            "RNAV 23": "https://aip.dfs.de/basicIFR/print/AD/EE5189F454BB293AEE13C5131322727C/AD%202%20EDDH%205-7-9",
+            "RNAV 33": "https://aip.dfs.de/basicIFR/print/AD/939450768C8643401AF91C3572568DE5/AD%202%20EDDH%205-7-12",
+
+        },
+        "STAR": {
+            "RNAV 05": "https://aip.dfs.de/basicIFR/print/AD/FEF6BED423A57F8DC11A6BC27D108D20/AD%202%20EDDH%203-1-1",
+            "RNAV 15": "https://aip.dfs.de/basicIFR/print/AD/D6734EEF481C14A38481D8EB569DB1EF/AD%202%20EDDH%203-1-4",
+            "RNAV 23": "https://aip.dfs.de/basicIFR/print/AD/CBD6FA0EF98AD1754A144DD32F840456/AD%202%20EDDH%203-1-7",
+            "RNAV 33": "https://aip.dfs.de/basicIFR/print/AD/9712BAB835853D196715A62AA883A8B9/AD%202%20EDDH%203-1-10",
+        },
+        "IAC": {
+            "ILS 05": "https://aip.dfs.de/basicIFR/print/AD/861D5B5739CC5B9D975564977AE50355/AD%202%20EDDH%204-2-1",
+            "ILS 15": "https://aip.dfs.de/basicIFR/print/AD/C63333F44594117277D3E892740051CC/AD%202%20EDDH%204-2-2",
+            "ILS 23": "https://aip.dfs.de/basicIFR/print/AD/18204AA21690E868A944E5E2CD449524/AD%202%20EDDH%204-2-3",
+            "RNP 33": "https://aip.dfs.de/basicIFR/print/AD/1C7F245B6504A19F4B0CAE6E89AFD05C/AD%202%20EDDH%204-6-10",
         }
     }
 }
